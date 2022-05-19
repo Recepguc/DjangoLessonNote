10bba6ebab76ed2fe9c27ce69fc6e198ad96641b
Recepguc <recep.gucc@gmail.com>
2022-05-19 23:04:41 +0300

    models lessons

--- model/main/urls.py
+++ model/main/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("fscohort.urls")),
+    path("", include("reletaions.urls")),
+
 ]
